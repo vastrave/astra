a73fd9c83997c1220f7674fc97068933cbbb94c1
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 19:39:50 -0400

    Update strap.sh

--- strap.sh
+++ strap.sh
@@ -29,8 +29,4 @@ EOF
 
 echo             
 echo "--> astra has finished setup."
-echo "--> Some changes require a reboot. The system will reboot automatically in 10s (press Ctrl-Z to stop)"
-sleep 10
-   sudo nvram "recovery-boot-mode=unused"
-   sudo reboot
-   
+echo
