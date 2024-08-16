fac1d36feadd3799eb33b193f5576c1877a82cad
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 13:01:50 -0400

    Update strap.sh

--- strap.sh
+++ strap.sh
@@ -15,6 +15,7 @@ cat > ~/astra/astra.conf <<EOF
 # astra config file
 
 # default config
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '~/astra/assets/desktop/background/desktop.png'"
 defaults write com.apple.dock orientation right
 defaults write com.apple.dock autohide-delay -float 0.5
 defaults write com.apple.dock autohide-time-modifier -float 0.8
