55308bb843121df9b0f1aeca475c3df6ab31811f
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 13:47:37 -0400

    Update strap.sh

--- strap.sh
+++ strap.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+echo         
 echo "\e[1;32m[->]\e[0m" "astra | v0.1"
 echo "Please note that astra may not work properly if you have SIP enabled."
 echo "This script will install some needed dependencies. Is that okay?"
+read
 read
 HOMEBREW_NO_AUTO_UPDATE=1 brew install clang
 HOMEBREW_NO_AUTO_UPDATE=1 brew install llvm
 HOMEBREW_NO_AUTO_UPDATE=1 brew install koekeishiya/formulae/yabai
                             # git clone --recursive https://github.com/AsahiLinux/m1n1.git
                             # make
+                            
 git clone https://github.com/satomiify/astra
 # sh ~/astra/conf.sh
 
