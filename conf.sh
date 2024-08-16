963c762ac6b20e32dfdcbfeb57565b39edb23f0a
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 13:24:19 -0400

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -2,4 +2,4 @@ sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set v
 defaults write com.apple.dock orientation right
 defaults write com.apple.dock autohide-delay -float 0.5
 defaults write com.apple.dock autohide-time-modifier -float 0.8
-EOF
+killall Dock
