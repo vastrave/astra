9a7b6bc5c9ca5152797ec9c65237d13eed30b32d
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 18:17:42 -0400

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -9,12 +9,13 @@ defaults write com.apple.menuextra.clock DateFormat HH:mm:ss
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 defaults write com.apple.CrashReporter UseUNC 1
 defaults write com.apple.universalaccess stickyKey 1
+defaults write com.apple.screencapture "type" -string "jpg"
 defbro org.mozilla.firefox
 
 # window management
 # yabai --start-service
 
-# dock
+# desktop
 sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '~/astra/assets/desktop/background/arch1.png'"
 
 defaults write com.apple.dock single-app -bool true
@@ -24,6 +25,7 @@ defaults write com.apple.dock orientation right
 defaults write com.apple.dock autohide-delay -float 0.1
 defaults write com.apple.dock autohide-time-modifier -float 0.8
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true"
 # defaults write com.apple.dock largesize -float 128
 
 # input
@@ -51,7 +53,11 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 # -- app store
 defaults write com.apple.appstore ShowDebugMenu -bool true
 
+# -- safari
+defaults write com.apple.Safari "ShowFullURLInSmartSearchField" -bool "true"
+
 killall Dock
    killall Finder
-      sudo pkill TouchBarServer
-         killall ControlStrip
+      killall Safari
+         sudo pkill TouchBarServer
+            killall ControlStrip
