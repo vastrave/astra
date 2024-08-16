a3667548d6927b963f5fa2c73e62085f34e53a68
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 13:37:13 -0400

    Update strap.sh

--- strap.sh
+++ strap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "\e[1;32m[->]\e[0m" astra | v0.1
+echo "\e[1;32m[->]\e[0m" "astra | v0.1"
 echo "Please note that astra may not work properly if you have SIP enabled."
 echo "This script will install some needed dependencies. Is that okay?"
 read
@@ -10,3 +10,5 @@ HOMEBREW_NO_AUTO_UPDATE=1 brew install llvm
                             # make
 git clone https://github.com/satomiify/astra
 sh ~/astra/conf.sh
+echo             
+echo "astra has finished setup. You are free to close terminal now. For information on how to configure and tweak, check the configuration guide."
