2d9ad9880b20afb7b9e2735848156dc65551dd31
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 13:29:51 -0400

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,5 +1,9 @@
+# astra config
+
+# default
 sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '~/astra/assets/desktop/background/arch1.png'"
 defaults write com.apple.dock orientation right
 defaults write com.apple.dock autohide-delay -float 0.1
 defaults write com.apple.dock autohide-time-modifier -float 0.8
+
 killall Dock
