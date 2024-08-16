831d811d2764ff2fe6bba2641a5735cc810fd5b7
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 12:10:02 -0400

    Update astra.sh

--- astra.sh
+++ astra.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-"\e[1;32m[->]\e[0m" astra
-echo "hi"
+echo "\e[1;32m[->]\e[0m" "astra"
 HOMEBREW_NO_AUTO_UPDATE=1 brew install llvm
 git clone --recursive https://github.com/AsahiLinux/m1n1.git
 make
