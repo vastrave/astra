76956a2d5043257d488058a24530ed9f10ed9e98
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 15:18:47 -0400

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -3,8 +3,10 @@
 
 # default
 
-# sys
+# system
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+defaults write com.apple.menuextra.clock DateFormat HH:mm:ss
+defaults write com.apple.LaunchServices LSQuarantine -bool false
 defaults write com.apple.CrashReporter UseUNC 1
 defaults write com.apple.universalaccess stickyKey 1
 defbro org.mozilla.firefox
@@ -24,6 +26,12 @@ defaults write com.apple.dock autohide-time-modifier -float 0.8
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 # defaults write com.apple.dock largesize -float 128
 
+# input
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticTextCompletionEnabled -bool false
+
 # apps
 # -- x/twitter
 defaults write com.twitter.twitter-mac TypeAnywhereToTweet -bool true
@@ -32,7 +40,13 @@ defaults write com.twitter.twitter-mac ESCClosesComposeWindow -bool true
 defaults write com.twitter.twitter-mac UserTimelineDerepeater -bool true
 
 # -- finder
-defaults write com.apple.finder QuitMenuItem -bool true; killall Finder
+defaults write com.apple.finder QuitMenuItem -bool true
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+   # search path
+   # This Mac : "SCev"
+   # Current Folder : "SCcf"
+   # Previous Path : "SCsp"
+   defaults write com.apple.finder FXDefaultSearchScope SCcf
 
 # -- app store
 defaults write com.apple.appstore ShowDebugMenu -bool true
