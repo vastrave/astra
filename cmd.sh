9d181ede085338d4d2aff96c35519c54dd09edcf
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 22:20:39 -0400

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 function astra() {
+echo "
+                     .::.
+                  .:'  .:     echo "astra v0.1"
+        ,MMM8&&&.:'   .:'     echo "by: satomiify"
+       MMMMM88&&&&  .:'       
+      MMMMM88&&&&&&:'         echo "device:" echo uname -r
+      MMMMM88&&&&&&
+    .:MMMMM88&&&&&&
+  .:'  MMMMM88&&&&
+.:'   .:'MMM8&&&'
+:'  .:'
+'::'  "
+
+
+function astra.apply() {
 sh ~/astra/conf.sh
 }
