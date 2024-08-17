6b28e550378c50982f4e3b48553ff3249633a21c
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 23:56:27 -0400

    Update strap.sh

--- strap.sh
+++ strap.sh
@@ -3,7 +3,7 @@
 echo         
 echo "-->""\e[1;32m[->]\e[0m" "astra | v0.1"
 echo "--> Please note that astra may not work properly if you have SIP enabled."
-echo "--> This script will install some needed dependencies. Is that okay?"
+echo "--> Several needed dependencies will be installed. Is that okay?"
 read
 read
 HOMEBREW_NO_AUTO_UPDATE=1 brew install clang-format@14
@@ -11,12 +11,13 @@ HOMEBREW_NO_AUTO_UPDATE=1 brew install llvm
 HOMEBREW_NO_AUTO_UPDATE=1 brew install llvm imagemagick
 # HOMEBREW_NO_AUTO_UPDATE=1 brew install firefox
 HOMEBREW_NO_AUTO_UPDATE=1 brew install koekeishiya/formulae/yabai
+HOMEBREW_NO_AUTO_UPDATE=1 brew install dockx
 HOMEBREW_NO_AUTO_UPDATE=1 brew install jwbargsten/misc/defbro
                             
 git clone https://github.com/satomiify/astra
 
-touch ~/astra/temp.sh
-cat > ~/astra/temp.sh <<EOF
+touch ~/astra/temp.txt
+cat > ~/astra/temp.txt <<EOF
 hwid
 EOF
 
