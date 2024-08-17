7b2eb11bc04df7c1e1392fe84e4394cfebad1d0e
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-16 23:39:16 -0400

    Update .cmd.sh

--- .cmd.sh
+++ .cmd.sh
@@ -9,7 +9,7 @@ echo "
         MMMMM88&&&&&&:'         OS: $(system_profiler SPSoftwareDataType | grep -o "macOS[^']*")
         MMMMM88&&&&&&           architecture: $(uname -m)
       .:MMMMM88&&&&&&           kernel: $(uname -or)
-    .:'  MMMMM88&&&&            memory $(system_profiler SPHardwareDataType | grep -Eo ":[^']*GB|TB")
+    .:'  MMMMM88&&&&            memory$(system_profiler SPHardwareDataType | grep -Eo ":[^']*GB|TB")
   .:'   .:'MMM8&&&'             terminal: $(echo $TERM_PROGRAM) ($(echo $TERM))
   :'  .:'
   '::'  
