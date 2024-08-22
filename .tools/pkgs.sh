e3f71897ce0db9e5c59cf0b4fbe40a4b55721d01
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-21 23:39:43 -0400

    Create pkgs.sh

---
+++ .tools/pkgs.sh
@@ -0,0 +1,9 @@
+ printf ' -> installing additional packages  '
+(while :; do for c in / - \\ \|; do printf '%s\b' "$c"; sleep 0.2; done; done) &
+   brew update
+   brew upgrade
+
+brew install llvm imagemagick python3 dockx jwbargsten/misc/defbro
+pip3 install keep
+# HOMEBREW_NO_AUTO_UPDATE=1 brew install firefox
+{ printf '\n'; kill $! && wait $!; } 2>/dev/null
