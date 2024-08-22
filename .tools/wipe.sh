f66a3d6f227d20b4a6d81f5e66eb95f66903bcc4
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-21 23:29:04 -0400

    Update wipe.sh

--- .tools/wipe.sh
+++ .tools/wipe.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+echo "wipe astra and its components?"
+echo "enter | y     ctrl-c | n"
+
+echo
+ printf ' -> wiping astra  '
+(while :; do for c in / - \\ \|; do printf '%s\b' "$c"; sleep 0.2; done; done) &
+
 sudo rm -rf ~/astra
 sudo rm -rf ~/saturn
 sudo rm -rf ~/.config/astra
+
+tmp="/tmp/astrap"
+if [ ! -d "$tmp" ]; then
+sudo rm -rf /tmp/astrap
+fi
+
+{ printf '\n'; kill $! && wait $!; } 2>/dev/null
