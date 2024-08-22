e3464b10007e9e72109fc3bb005278e903509997
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-21 23:48:46 -0400

    Update wipe.sh

--- .tools/wipe.sh
+++ .tools/wipe.sh
@@ -2,10 +2,11 @@
 
 echo "wipe astra and its components?"
 echo "enter | y     ctrl-c | n"
+read
 
 echo
  printf ' -> wiping astra  '
-(while :; do for c in / - \\ \|; do printf '%s\b' "$c"; sleep 0.2; done; done) &
+(while :; do for c in / - \\ \|; do printf '\b%s' "$c"; sleep 0.2; done; done) &
 
 sudo rm -rf ~/astra
 sudo rm -rf ~/saturn
