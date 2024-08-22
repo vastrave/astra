cd29a69b2a878094393b3fd8923762d99e71c7f0
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-21 23:11:29 -0400

    Update strap.sh

--- install/strap.sh
+++ install/strap.sh
@@ -66,7 +66,7 @@ tmp=/tmp/astrap
 #       echo "--> astrap needs to run as root;"
 #         echo "  please enter your password if prompted."
 #           echo
-             caffeinate -dis sudo -E sh .astrap.sh
+#             caffeinate -dis sudo -E sh .astrap.sh
 #    else
 #        caffeinate -dis sudo -E sh .astrap.sh
 #    fi
