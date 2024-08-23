0cc683b5ceb8799179c8c817d060276ce2ec2631
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-23 12:43:14 -0400

    Update wipe.sh

--- .tools/wipe.sh
+++ .tools/wipe.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 echo " -> wipe astra and its components?"
 echo "    enter | y     ctrl-c | n"
 read
@@ -7,6 +9,32 @@ read
 echo
  printf ' -> wiping astra  '
 (while :; do for c in / - \\ \|; do printf '\b%s' "$c"; sleep 0.2; done; done) &
+
+if [ ! -e ~/astra ]; then
+      echo
+      echo " Exiting with error:"
+      echo "  *Could not find the astra folder. Did you move it?"
+      echo $i
+else
+      echo
+fi
+
+if [ ! -e ~/saturn ]; then
+      echo
+      echo " Exiting with error:"
+      echo "  *Could not find the saturn folder. Did you move it?"
+      exit 1
+else
+      echo 
+fi
+
+if [ ! -e ~/.config/astra ]; then
+      echo
+      echo " Exiting with error:"
+      echo "  *Could not find the astra configuration folder. Did you move it?"
+else
+      echo 
+fi
 
 sudo rm -rf ~/astra
 sudo rm -rf ~/saturn
