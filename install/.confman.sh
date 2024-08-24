66043f0d54e3ad5ac3e50a6e086aa9858d140795
vastra <92891760+satomiify@users.noreply.github.com>
2024-08-24 12:35:01 -0400

    Update .confman.sh

--- install/.confman.sh
+++ install/.confman.sh
@@ -16,10 +16,12 @@ read -r -p " --> What is your name?: " usrn
    echo "     (You can find images of each theme at https://github.com/vastrave/astra)"
 
 echo
-   $usrm=read -r -p "  1. Modern" th1
+# $usrm=
+   read -r -p "  1. Modern" th1
      if $th1; then
      echo "  ! Would you like to install: \033[1;34mModern\033[0m?"
-     $tcd=read
+     # $tcd
+     read
      sleep 0.8
      echo "  ! Your preference has been saved. astra will install as [\033[1;34mModern\033[0m] later."
      fi
@@ -31,10 +33,12 @@ echo
    echo "     updates some values that mess with them."
 
    echo
-   $usrg=read -r -p "  1. AMD" gc1
+   # $usrg
+   read -r -p "  1. AMD" gc1
      if $gc1; then
      echo "  ! Is this correct?: \033[0;31mAMD\033[0m"
-     $gcd=read
+     # $gcd
+     read
      sleep 0.8
      echo "    Understood. Saved graphics card as \033[0;31mAMD\033[0m."
      fi
@@ -46,10 +50,12 @@ echo
    echo "     expand this as time goes.)"
 
    echo
-   $usrl=read -r -p "  1. English" lg1
+   # $usrl=
+   read -r -p "  1. English" lg1
      if $lg1; then
      echo "  ! So, install astra in English?"
-     $lcd=read
+     # $lcd=
+     read
      sleep 0.8
      echo "    Understood. astra will be installed in English."
      fi
