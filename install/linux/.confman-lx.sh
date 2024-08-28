366754deec623a0906e8578a68d7bc146cf202be
vastra <92891760+satomiify@users.noreply.github.com>
2024-08-27 20:37:27 -0400

    Create .confman-lx.sh

---
+++ install/linux/.confman-lx.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# mode: auto
+
+clear
+echo
+read -r -p " --> What is your name?: " usrn
+   echo "     Please wait.."
+    sleep 1.5
+    clear
+   echo
+   echo "     astra Interactive Pre-Installation Setup"
+   sleep 2
+   echo "     Welcome."
+    sleep 3
+    echo
+   echo "     First of all, which theming do you prefer for astra?"
+   echo "     (You can find images of each theme at https://github.com/vastrave/astra)"
+
+echo
+# $usrm=
+   read -r -p "  1. Modern" th1
+     if $th1; then
+     echo -e "  ! Would you like to install: \033[1;34mModern\033[0m?"
+     # $tcd
+     read
+     sleep 0.8
+     echo -e "  ! Your preference has been saved. astra will install as [\033[1;34mModern\033[0m] later."
+     fi
+
+     sleep 3
+     echo
+   echo "     Now, please select your graphics card below."
+   echo "     This is needed if you want proper graphic rendering, as astra"
+   echo "     updates some values that mess with them."
+
+   echo
+   # $usrg
+   read -r -p "  1. AMD" gc1
+     if $gc1; then
+     echo -e "  ! Is this correct?: \033[0;31mAMD\033[0m"
+     # $gcd
+     read
+     sleep 0.8
+     echo -e "    Understood. Saved graphics card as \033[0;31mAMD\033[0m."
+     fi
+
+       sleep 3
+     echo
+   echo "     Lastly, what language should astra be installed in?"
+   echo "     (Currently, only English is supported, though I hope to"
+   echo "     expand this as time goes.)"
+
+   echo
+   # $usrl=
+   read -r -p "  1. English" lg1
+     if $lg1; then
+     echo "  ! So, install astra in English?"
+     # $lcd=
+     read
+     sleep 0.8
+     echo "    Understood. astra will be installed in English."
+     fi
+
+     sleep 4
+     echo "    Thank you for your time."
+     sleep 2
+     echo "    Begin the installation process now?"
+     read
+
+     sleep 2
+
+     clear
+
+          if [ "$USER" != "root" ]; then
+     echo
+       echo "--> astrap needs to run as root;"
+         echo "  please enter your password if prompted."
+           echo
+             systemd-inhibit sudo -E sh .astrap-linux.sh
+    else
+        systemd-inhibit sudo -E sh .astrap-linux.sh
+    fi
