2a1432ebe703d7209b5f218d3228a7aec241983b
vastra <92891760+satomiify@users.noreply.github.com>
2024-08-27 20:44:55 -0400

    Update pkgs.sh

--- .tools/pkgs.sh
+++ .tools/pkgs.sh
@@ -1,5 +1,7 @@
  printf ' -> installing additional packages  '
 (while :; do for c in / - \\ \|; do printf '\b%s' "$c"; sleep 0.2; done; done) &
+
+if [ -e "/System" ]; then
    brew update
 #  brew upgrade
 
@@ -7,4 +9,8 @@ brew install --quiet llvm imagemagick python3 dockx jwbargsten/misc/defbro dmg2i
 brew install --quiet --cask marta menubarx
 pip3 install -q keep
 # HOMEBREW_NO_AUTO_UPDATE=1 brew install firefox
+fi
+
+if [ -e "/root" ]; then
+sleep 1 # define
 { printf '\n'; kill $! && wait $!; } 2>/dev/null
